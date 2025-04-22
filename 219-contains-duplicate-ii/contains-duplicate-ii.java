cd0d695ee9745ca086f87a54ee013c8095837b52
Shubham <118046038+shubham4349@users.noreply.github.com>
2025-04-22 15:41:36 +0530

    Time: 21 ms (46.44%) | Memory: 61 MB (5.57%) - LeetSync

--- 219-contains-duplicate-ii/contains-duplicate-ii.java
+++ 219-contains-duplicate-ii/contains-duplicate-ii.java
@@ -3,13 +3,13 @@ class Solution {
         HashMap<Integer,Integer> map = new HashMap<>();
         for(int i=0;i<nums.length;i++){
             if(map.containsKey(nums[i])){
-                int ans = Math.abs(i-map.get(nums[i]));
+                int ans = Math.abs(i-map.get(nums[i])); // get wala method nums[i] pe jaake i return kr rha
                 if(ans <= k){
                     return true;
                 }
             }
             // else{
-                map.put(nums[i],i);
+                map.put(nums[i],i); // ye key, value ki pair bana rha
             // }
         }
         // if(ans <= k){

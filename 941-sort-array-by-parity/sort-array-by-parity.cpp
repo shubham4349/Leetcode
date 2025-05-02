66f1ee5737a8d778c237c3f897e92bfb38fd988a
Shubham <118046038+shubham4349@users.noreply.github.com>
2025-05-02 22:51:14 +0530

    Time: 0 ms (100.00%) | Memory: 20.5 MB (90.76%) - LeetSync

--- 941-sort-array-by-parity/sort-array-by-parity.cpp
+++ 941-sort-array-by-parity/sort-array-by-parity.cpp
@@ -3,17 +3,27 @@ public:
     vector<int> sortArrayByParity(vector<int>& nums) {
         
         int n = nums.size();
-
-        vector<int> temp;
-
-        for(int i =0; i<n; i++){
-            if(nums[i]%2==0) temp.push_back(nums[i]);
-        }
         
-        for(int i =0; i<n; i++){
-            if(nums[i]%2!=0) temp.push_back(nums[i]);
-        }
-        return temp;
+        int start = 0;
+        int end = n-1; 
 
+       // Approach-
+    //    start me agar even ho to start++,
+    //    end me agar odd ho to end--;
+    //    else (yani agar start me odd ho ya end me even)-> then
+    //    swap those with start++, end--
+        while(start<end){
+            if(nums[start]%2==0){ // jab start pe even ho, skip
+                start++;
+            }
+            else if(nums[end]%2!=0){ // jab end me odd ho, skip
+                   end--;
+            } 
+            else {
+                swap(nums[start], nums[end]);
+                start++; end--;
+            }
+        }
+        return nums;
     }
 };

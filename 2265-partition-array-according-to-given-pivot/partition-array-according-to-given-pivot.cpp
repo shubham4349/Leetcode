8c3c7884eccc0a5bb233627036fd998d9bf8cf88
Shubham <118046038+shubham4349@users.noreply.github.com>
2025-05-02 22:40:19 +0530

    Time: 12 ms (22.11%) | Memory: 143.9 MB (5.51%) - LeetSync

--- 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.cpp
+++ 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.cpp
@@ -4,21 +4,14 @@ public:
         
         int n = nums.size();
 
-         vector<int> less;
+         vector<int> less, pivott, more;
 
          for(int i =0; i<n; i++){
             if(nums[i]<pivot) less.push_back(nums[i]);
+            else if(nums[i]==pivot) pivott.push_back(nums[i]);
+            else more.push_back(nums[i]);
          }
      
-         
-         vector<int> pivott;
-         for(int i =0; i<n; i++) {
-            if(nums[i]==pivot) pivott.push_back(nums[i]);
-         }
-             vector<int> more;
-         for(int i =0; i<n; i++){
-            if(nums[i]>pivot)  more.push_back(nums[i]);
-         }
          vector<int> output;
 
          for(int i=0; i<less.size(); i++){

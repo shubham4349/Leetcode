2c24d1ca07f95ce7a6f55f60723d5180788134cc
Shubham <118046038+shubham4349@users.noreply.github.com>
2025-05-16 01:28:46 +0530

    Time: 826 ms (5.64%) | Memory: 13.7 MB (26.57%) - LeetSync

---
+++ 137-single-number-ii/single-number-ii.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        
+         int n = nums.size();
+        
+        vector<int> temp(n,-1111);
+
+        for(int i =0; i<n; i++){
+            for(int j =0; j<n; j++){
+                if(i!=j and nums[i]==nums[j]) {
+                    temp[i] = nums[i];
+                }
+            }
+        }
+       int ans;
+
+        for(int i=0; i<n; i++){
+            if(temp[i]==-1111){
+                ans = nums[i];
+                return ans;
+            }
+        }
+        return -1;
+    }
+};

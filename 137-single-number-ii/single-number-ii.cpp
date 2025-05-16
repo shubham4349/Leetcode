5dde63ccb5caee230c99744daa76dad608c30b9a
Shubham <118046038+shubham4349@users.noreply.github.com>
2025-05-16 14:00:34 +0530

    Time: 831 ms (5.64%) | Memory: 13.6 MB (26.79%) - LeetSync

--- 137-single-number-ii/single-number-ii.cpp
+++ 137-single-number-ii/single-number-ii.cpp
@@ -4,7 +4,7 @@ public:
         
          int n = nums.size();
         
-        vector<int> temp(n,-1111);
+        vector<int> temp(n,-6969);
 
         for(int i =0; i<n; i++){
             for(int j =0; j<n; j++){
@@ -16,7 +16,7 @@ public:
        int ans;
 
         for(int i=0; i<n; i++){
-            if(temp[i]==-1111){
+            if(temp[i]==-6969){
                 ans = nums[i];
                 return ans;
             }

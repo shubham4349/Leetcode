7987924bd8537c4eb9a31dfcff2dff493b1f31cb
Shubham <118046038+shubham4349@users.noreply.github.com>
2025-05-18 12:21:14 +0530

    Time: 0 ms (100.00%) | Memory: 20.7 MB (27.86%) - LeetSync

--- 136-single-number/single-number.cpp
+++ 136-single-number/single-number.cpp
@@ -1,23 +1,12 @@
-#include <iostream>
-#include <vector>
-using namespace std;
-
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
         int n = nums.size();
         
-        for(int i =0; i<n;i++){
-            int count =0;
-
-            for(int j =0; j<n; j++){
-                if(i!=j and nums[i]==nums[j]){
-                    count++;
-                }
-
-            }
-            if(count==0) return nums[i];
+        int ans = 0;
+        for(int i =0; i<n; i++){
+            ans^=nums[i];
         }
-        return -1;
+        return ans;
     }
 };

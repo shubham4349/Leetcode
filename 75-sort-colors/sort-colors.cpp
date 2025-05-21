4f465a855190f34f483378c4dcb35491476743f6
Shubham <118046038+shubham4349@users.noreply.github.com>
2025-05-21 14:22:51 +0530

    Time: 0 ms (100.00%) | Memory: 11.6 MB (49.22%) - LeetSync

--- 75-sort-colors/sort-colors.cpp
+++ 75-sort-colors/sort-colors.cpp
@@ -2,7 +2,30 @@ class Solution {
 public:
     void sortColors(vector<int>& nums) {
         
-        sort(nums.begin(), nums.end());
+       int n =nums.size();
+
+       int count0 =0, count1= 0, count2=0;
+
+       for(int i =0; i<n; i++){
+          if(nums[i]==0) count0++;
+          else if(nums[i]==1) count1++;
+          else count2++;
+       }
+
+       // ab 0 ko uske count tak fill kro nums me:
+
+       for(int i =0; i<count0; i++){
+           nums[i]=0;
+       }
+     // ab 1 ko uske count0+count1(yani 2 idx se 4 tak) fill kro nums me:
+       for(int i =count0; i< count0 +count1; i++){
+        nums[i]=1;
+       }
+       // vice versa
+       for(int i =count0+count1; i<n; i++){
+        nums[i] =2;
+       }
+
         
     }
 };

d362781916891074c9d7722409163e6d759b9b0a
Shubham <118046038+shubham4349@users.noreply.github.com>
2025-05-22 23:54:29 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (40.49%) - LeetSync

--- 1889-check-if-number-is-a-sum-of-powers-of-three/check-if-number-is-a-sum-of-powers-of-three.cpp
+++ 1889-check-if-number-is-a-sum-of-powers-of-three/check-if-number-is-a-sum-of-powers-of-three.cpp
@@ -1,40 +1,6 @@
 class Solution {
 public:
     bool checkPowersOfThree(int n) {
-        
-        // int x;
-        // for(int i =0; i<=16; i++){
-        //     if(pow(3,i)>=n){
-        //         x = i-1;
-        //         break;
-        //     } 
-        // }
-        // // yani n=91 ke liye, x hmara 4 hua as, 3^4 = 81;
-        // // to uske ke liye hm maxm 4 power tak jayenge;
-
-        //  int diff = n- pow(3,x); // 10
-        //  // so ab chek kr ki if diff can be written in power of threes or not.
-        // for(int i =0; i<=x; i++){
-
-           
-        // }
-        // while(diff>=pow)
-
-
-        // while(n>1){
-        //     n = n/3;
-        //     if(n<3 and n!=1) return false;
-        // }
-        // return true;
-        // int x;
-        // for(int i =0; i<=16; i++){
-        //     if(n==pow(3,i)) return true;
-        //     else if(n<=pow(3,i)) x = i-1;
-        // }
-        // for(int i =0; i<=x; i++){
-
-        // }
-        // if(n%3==2 or n%3==0)
 
         // finding ternary representation of N
         long long rem=0;
@@ -56,7 +22,7 @@ public:
         }
         return true;
 
-
+        // just looked up what ternary repre is, and coded it all by myself \U0001f601
     }
 
 };

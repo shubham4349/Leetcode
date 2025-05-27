ec09f2c407369337164886d85652514a71cbf217
Shubham <118046038+shubham4349@users.noreply.github.com>
2025-05-27 10:49:01 +0530

    Time: 2 ms (75.90%) | Memory: 7.9 MB (22.89%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -2,17 +2,15 @@ class Solution {
 public:
     
     int fib(int n) {
-     
-   if(n<=1) return n;
-
-    vector<int> dp(n+1);
-    // tabulation: O(n)
-    dp[0]=0;
-    dp[1]=1;
-    for(int i =2; i<=n; i++){
-        dp[i]=dp[i-1]+dp[i-2];
-    }
-     return dp[n];
+       if(n<=1) return n;
+       int a=0, b=1, c;
+       // space optimised tabulation:
+       for(int i =2; i<=n; i++){
+         c = a+b;
+         a=b;
+         b=c;
+       }
+       return c;
        
     }
 };

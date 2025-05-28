30de6bb220088cd56ad567791c1badf42fd7d843
Shubham <118046038+shubham4349@users.noreply.github.com>
2025-05-28 13:56:25 +0530

    Time: 1 ms (80.91%) | Memory: 14.9 MB (21.70%) - LeetSync

---
+++ 1-two-sum/two-sum.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int n = nums.size();
+         unordered_map<int,int> map;
+        // hasmaps approach:
+        // traverse the array.
+        // for every nums[i] check if (target-nums[i]) exist in array or not.  
+        // jese for 2 (check 9-2 = 7 exist or not) by that u'll find ans
+
+        for(int i =0; i<n; i++){
+            int comp = target-nums[i];
+
+            if(map.find(comp)!=map.end()){
+                return  {map[comp],i};
+            }
+            map[nums[i]]=i;
+        }
+        return {};
+    }
+    // map me pehle 2,0 aaya.
+    // then 7 ke liye comp find kiya (2) aur vo map me mila
+    // so 2 aur 7 ka idx return kradiya
+};

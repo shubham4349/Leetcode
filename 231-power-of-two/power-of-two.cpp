aba133bd5ac96c6899f00822c0e49f799a031ae3
Shubham <118046038+shubham4349@users.noreply.github.com>
2025-05-29 01:27:07 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (18.50%) - LeetSync

--- 231-power-of-two/power-of-two.cpp
+++ 231-power-of-two/power-of-two.cpp
@@ -2,16 +2,32 @@
      class Solution {
 public:
     bool isPowerOfTwo(int n) {
-        if (n <= 0) return false;
-        
-         long long power =1;
 
-        while(power <n) {
-            power *= 2; // power ko 2 se multiply krte jao
-        }
+        if(n<=0) return false;
+        //using bit manipulation
 
-        if(power==n) return true;
-        return false;
+         // concept
+         // power of 2 h only 1 set bit;
+             // 2 = 10
+            // 4 = 100
+            // 8 = 1000
+            // 16 = 10000 
+
+    // if n is a power of 2 then->(n & n-1 ==0)
+
+    //    n      = 8  = 1000
+    //    n - 1  = 7  = 0111
+
+    // now perform bitwise and (multiply) 
+
+    // n        = 1000
+    // n - 1    = 0111
+    // ----------------
+    // AND      = 0000
+
+
+    return  n>0 and (n & (n-1))==0;
+
     }
 };
 

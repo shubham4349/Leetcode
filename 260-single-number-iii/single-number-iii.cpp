a154d02e53a38608c86453d0ae5ab8799dad8f39
Shubham <118046038+shubham4349@users.noreply.github.com>
2025-05-31 13:08:36 +0530

    Time: 4 ms (19.57%) | Memory: 15 MB (13.69%) - LeetSync

--- 260-single-number-iii/single-number-iii.cpp
+++ 260-single-number-iii/single-number-iii.cpp
@@ -1,42 +1,18 @@
 class Solution {
 public:
     vector<int> singleNumber(vector<int>& nums) {
-        int n = nums.size();
-        
-        vector<int> temp(n,-1);
+       
+    unordered_map<int,int> mp;
 
-        for(int i =0; i<n; i++){
-            for(int j =0; j<n; j++){
-                if(i!=j and nums[i]==nums[j]) {
-                    temp[i] = nums[i];
-                }
-            }
-        }
-        vector<int> ans;
+    for(int i =0; i<nums.size(); i++){
+        mp[nums[i]]++;
+    }
+    vector<int> ans;
+     for(int i =0; i<nums.size(); i++){
+        if(mp[nums[i]]==1) ans.push_back(nums[i]);
+    }
 
-        for(int i=0; i<n; i++){
-            if(temp[i]==-1){
-                ans.push_back(nums[i]);
-            }
-        }
-
-        // for(int i =0; i<n; i++){
-        //      for(int j = 0; j<temp.size(); j++){
-        //         if()
-        //      }
-        // }
-        
-        // for(int i =0; i<n; i++){
-        //     int count = 0;
-        //     for(int j =0; j<n; j++){
-                
-        //         if(i!=j and nums[i]==nums[j]) count++;
-
-        //         if(count==0) temp.push_back(nums[i]);
-        //     }
-        // }
-
-   return ans;
+    return ans;
 
     }
 };

d2469be0fd32ba615232247bcaa3d8e80301e735
Shubham <118046038+shubham4349@users.noreply.github.com>
2025-06-10 13:57:20 +0530

    Time: 7 ms (5.86%) | Memory: 9.9 MB (27.18%) - LeetSync

--- 3753-maximum-difference-between-even-and-odd-frequency-i/maximum-difference-between-even-and-odd-frequency-i.cpp
+++ 3753-maximum-difference-between-even-and-odd-frequency-i/maximum-difference-between-even-and-odd-frequency-i.cpp
@@ -6,25 +6,21 @@ public:
 
         int n= s.length();
         int max_odd=INT_MIN, min_even=INT_MAX;
+
         unordered_map<char,int> freq;
         for(int i =0; i<n; i++){
             freq[s[i]]++;
         }
+
         for(int i =0; i<n; i++){
             if(freq[s[i]]%2==0){ // even rhne pe min even nikal
                 min_even= min(min_even, freq[s[i]]);
             }else{ // odd pe max odd
-                   max_odd= max(max_odd, freq[s[i]]);
+                max_odd= max(max_odd, freq[s[i]]);
             }
         }
-          int a1,a2;
-        for(int i=0; i<n; i++){
-            if(freq[s[i]]==max_odd) a1 = freq[s[i]];
-            else if(freq[s[i]]==min_even) a2= freq[s[i]];
-        }
-
-         return a1-a2;
-        // int n=s.length();
+          
+          return max_odd -min_even;
 
     }
 };

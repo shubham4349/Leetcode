c8ff53110597e57f672851a551b3e62915e92bc4
Shubham <118046038+shubham4349@users.noreply.github.com>
2025-06-12 00:31:36 +0530

    Time: 0 ms (100.00%) | Memory: 9.9 MB (11.94%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -5,21 +5,22 @@ public:
          int n= s.length();
          int l = t.length();
 
-         unordered_map<char,int> mp1, mp2;
+         unordered_map<char,int> mp1 ; 
 
-         for(int i=0; i<n; i++) mp1[s[i]]++;
-         for(int i=0; i<l; i++) mp2[t[i]]++;
-        
-         int large = ( n>l ? n : l);
-
-         string big;
-         big = (n>l ? s : t);
-
-         for(int i =0; i<large; i++){
-            // if(mp1.count(t[i])==0) return false; // nhi contain krta to false return
-            if(mp1[big[i]]!=mp2[big[i]]) return false;
+         for(int i =0; i<n; i++){
+            mp1[s[i]]++;
          }
-        return true;
+          for(int i =0; i<l; i++){
+            mp1[t[i]]--;
+         }
 
+         int big = (n>l? n:l);
+         string bigstring = (n>l? s:t);
+         for(int i =0; i<big; i++){
+            if(mp1[bigstring[i]]!=0) return false;
+         }
+         return true;
+         
+         
     }
 };

6d3729e794e8d2b22a4541b35a68d495c81ddcf2
Shubham <118046038+shubham4349@users.noreply.github.com>
2025-06-14 18:59:16 +0530

    Time: 55 ms (26.71%) | Memory: 67 MB (23.78%) - LeetSync

--- 442-find-all-duplicates-in-an-array/find-all-duplicates-in-an-array.cpp
+++ 442-find-all-duplicates-in-an-array/find-all-duplicates-in-an-array.cpp
@@ -3,16 +3,15 @@ public:
     vector<int> findDuplicates(vector<int>& nums) {
          int n=nums.size();
 
-         unordered_map<int,int> count;
+        unordered_map<int,int> count;
 
-         for(int i=0; i<n; i++) count[nums[i]]++;
-         set<int> hello;
-         for(int i=0; i<n; i++){
-            if(count[nums[i]]==2) {
-                if(hello.count(nums[i])==0) hello.insert(nums[i]); 
-            }
-         }
-         vector<int> ans(hello.begin(), hello.end());
-         return ans;
+         for(int i=0; i<n; i++) count[nums[i]]++; 
+      
+        // bete jb hasmap use kiya h to usi ko iterate kiya kr-> TC kam aayegi
+          vector<int> ans;
+          for(auto it : count) {
+            if(it.second ==2) ans.push_back(it.first);
+          }
+          return ans;
     }
 };

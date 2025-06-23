3d8a60256056499c53daaa8da69fc80d07628d33
Shubham <118046038+shubham4349@users.noreply.github.com>
2025-06-23 22:57:03 +0530

    Time: 0 ms (100.00%) | Memory: 11.1 MB (47.83%) - LeetSync

--- 2436-make-array-zero-by-subtracting-equal-amounts/make-array-zero-by-subtracting-equal-amounts.cpp
+++ 2436-make-array-zero-by-subtracting-equal-amounts/make-array-zero-by-subtracting-equal-amounts.cpp
@@ -1,47 +1,15 @@
 class Solution {
 public:
-    // int smallestNonZero( vector<int>& nums){
-    //     int snz; // smallest non zero
-    //     int n=nums.size();
-    //     for(int i=0; i<n; i++){
-    //           if(nums[i]!=0){
-    //             snz=nums[i];
-    //             break;
-    //           }
-    //     }
-    //     return snz;
-
-
-    // }
     int minimumOperations(vector<int>& nums) {
         
         int n=nums.size();
+        unordered_map<int,int> count;
 
-        // x<= smallest non zero 
-
-        // case 1 = x = smallest non zero 
-        //
-    //     if(n==1 and nums[0]==0) return 0;
-    //     sort(nums.begin(), nums.end());
-    //     int sum=accumulate(nums.begin(),nums.end(),0);
-    //     int count=0;
-    //     while(sum!=0){
-    //         for(int i=0; i<n; i++){
-    //             if(nums[i]!=0) nums[i] -= smallestNonZero(nums);
-    //             sum +=nums[i];
-    //         }
-    //         count++;
-    //         //sum=0; 
-    //     }
-    //    return count;
-
-        // hint-3: 
-        set<int> hello(nums.begin(),nums.end()); 
-
-        if(*hello.begin()!=0) return hello.size();
-        else return hello.size()-1;
-
-        return -1; 
-
+        for(auto i : nums) count[i]++;
+        int ans=0;
+        for(auto it: count){
+              if(it.first !=0) ans++;
+        } 
+        return ans;
     }
 };

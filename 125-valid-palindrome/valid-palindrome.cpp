4894a2a8d9f241871f386729184e090a25634046
Shubham <118046038+shubham4349@users.noreply.github.com>
2025-07-07 20:41:38 +0530

    Time: 3 ms (31.70%) | Memory: 10.6 MB (12.46%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.cpp
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -2,29 +2,13 @@ class Solution {
 public:
     bool isPalindrome(string s) {
         int n=s.length();
-        // alphanumneric yani- numbers+ alphabets ka set bana
-        unordered_set<char> lower; // lowercase alphabets
-        char ch='a';
-        while(ch!='z') {
-            lower.insert(ch);
-            ch++;
-        }
-        lower.insert(ch); // z ko insert kiya
-        ch='0';
-        while(ch!='9') {
-            lower.insert(ch);
-            ch++;
-        }
-        lower.insert(ch); // 9 ko inert kiye
-        unordered_set<char> given;
-        string temp="";
+        // approahc:2 -> using builtin method
+        string temp;
         for(int i=0; i<n; i++){
-             char is = tolower(s[i]);
-             if(lower.find(is)!=lower.end()) temp+= is; // agar alphabet h to nye string me push kro
+            if(isalnum(s[i])) temp+= tolower(s[i]);
         }
-        string orig = temp;
+        string orig=temp;
         reverse(temp.begin(), temp.end());
-        return temp==orig;
-
+        return orig==temp;
     }
 };

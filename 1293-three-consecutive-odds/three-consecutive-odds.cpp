8e821f9398ae380267b07dc02b0c74534d3feb06
Shubham <118046038+shubham4349@users.noreply.github.com>
2025-07-13 16:57:36 +0530

    Time: 0 ms (100.00%) | Memory: 12.1 MB (18.76%) - LeetSync

--- 1293-three-consecutive-odds/three-consecutive-odds.cpp
+++ 1293-three-consecutive-odds/three-consecutive-odds.cpp
@@ -5,16 +5,9 @@ public:
        
          int count = 0;
        for(int i =0; i<n; i++){
-           
-            if(arr[i]%2!=0){
-                count++;
-                if(count==3) return true;
-
-            }
-            else{
-                count = 0;
-                
-            }
+            if(arr[i]%2==1) count++;
+            else count=0;
+            if(count==3) return true;
        }
        return false;
     }

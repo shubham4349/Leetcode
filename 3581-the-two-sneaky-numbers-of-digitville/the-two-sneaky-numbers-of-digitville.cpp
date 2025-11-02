b8c0fdf4e35dbdab32f3e5b12b66698770fdc937
Shubham <118046038+shubham4349@users.noreply.github.com>
2025-11-02 13:08:16 +0530

    Time: 0 ms (100.00%) | Memory: 26.4 MB (30.34%) - LeetSync

--- 3581-the-two-sneaky-numbers-of-digitville/the-two-sneaky-numbers-of-digitville.cpp
+++ 3581-the-two-sneaky-numbers-of-digitville/the-two-sneaky-numbers-of-digitville.cpp
@@ -5,8 +5,11 @@ public:
         map<int,int> count;
         for(int num: nums) count[num]++;
          vector<int> ans;
-        for(auto it: count){
-            if(it.second>1) ans.ghusao(it.first);
+        // for(auto it: count){
+        //     if(it.second>1) ans.ghusao(it.first);
+        // }
+        for(auto &[key,val]:count){
+            if(val>1) ans.ghusao(key);
         }
         return ans;
     }

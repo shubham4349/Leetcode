53c451464c68d3c9b43f7a8999eaa35e1d87177d
Shubham <118046038+shubham4349@users.noreply.github.com>
2025-11-05 20:01:13 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (57.93%) - LeetSync

--- 3447-clear-digits/clear-digits.cpp
+++ 3447-clear-digits/clear-digits.cpp
@@ -1,32 +1,13 @@
 class Solution {
-public: 
-         // approach1-> brute
-         // isdigit-> builtin h
-    bool isDigit(char ch){
-         if(ch>='0' and ch<='9') return true;
-         return false;
-    }
-    string deleteNonDigit(int idx, string &s){
-        for(int i=idx; i>=0; i--){
-            if(!isDigit(s[i]) and s[i]!='.'){
-                 s[i]='.'; // marking
-                 break;
-            }
-        }
-        return s;
-    }
+public:
     string clearDigits(string s) {
         
-        int n=s.length();
-        for(int i=0; i<n; i++){
-           if(isDigit(s[i])){
-               s[i]='.';// marking
-               deleteNonDigit(i-1,s);
-           }
-        }
+        // approach->2;
+        // using string stack like mthods
         string ans;
         for(char ch:s){
-          if(ch!='.')ans.push_back(ch);
+            if(isdigit(ch)) ans.pop_back(); // sbse latest dala hua non-digit delete
+            else ans.push_back(ch);
         }
         return ans;
     }

e9b8fb0841229f20f38b00755046a256f13a6f3c
Shubham <118046038+shubham4349@users.noreply.github.com>
2025-11-23 15:16:42 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (96.70%) - LeetSync

--- 1960-check-if-the-sentence-is-pangram/check-if-the-sentence-is-pangram.cpp
+++ 1960-check-if-the-sentence-is-pangram/check-if-the-sentence-is-pangram.cpp
@@ -1,11 +1,18 @@
 class Solution {
 public:
     bool checkIfPangram(string sentence) {
+        // method 1
         string alpha="abcdefghijklmnopqrstuvwxyz";
-        set<char> s(sentence.begin(),sentence.end());
-        string ans;
-        ans.assign(s.begin(),s.end());
-        return ans==alpha;
+        // set<char> s(sentence.begin(),sentence.end());
+        // string ans;
+        // ans.assign(s.begin(),s.end());
+        // return ans==alpha;
+
+        // method 2
+        for(int i=0; i<26; i++){
+            if(sentence.find(alpha[i])==string::npos) return false;
+        }
+        return true;
 
     }
 };

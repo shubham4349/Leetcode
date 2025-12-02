ee504b5c843d2fd11873d7f3aeb1c6f245403d0c
Shubham <118046038+shubham4349@users.noreply.github.com>
2025-12-02 17:29:23 +0530

    Time: 85 ms (8.33%) | Memory: 215.5 MB (8.33%) - LeetSync

---
+++ 3526-minimum-number-of-flips-to-make-binary-grid-palindromic-i/minimum-number-of-flips-to-make-binary-grid-palindromic-i.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    int minFlips(vector<vector<int>>& grid) {
+         int r=grid.size();
+         int c=grid[0].size();
+        // pehle row ko pali banao, and steps cnt kr
+        // then col ko similarly cnt
+        // he return the minm cnt
+        vector<vector<int>> gridd=grid;
+        int rowcnt=0;
+        for(int i=0; i<r; i++){
+            int x=0;
+            int y=c-1;
+            while(x<=y){
+                if(grid[i][x]!=grid[i][y]){
+                    grid[i][x]=grid[i][y];
+                    grid[i][y]=grid[i][x];
+                    rowcnt++;  
+                }
+                x++;y--;
+            }
+        }
+        int colcnt=0;
+        for(int j=0; j<c; j++){
+            int x=0;
+            int y=r-1;
+            while(x<=y){
+                if(gridd[x][j]!=gridd[y][j]){ // gridd isliye kyuki row opn ke bad grid ke elements change hogye
+                   gridd[x][j]=gridd[y][j];
+                   gridd[y][j]=gridd[x][j];
+                    colcnt++;  
+                }
+                x++;y--;
+            }
+        }
+        return min(rowcnt,colcnt);
+    }
+};

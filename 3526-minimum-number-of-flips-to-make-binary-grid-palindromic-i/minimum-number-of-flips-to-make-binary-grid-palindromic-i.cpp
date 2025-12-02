982126a764d191f8a19fb550482d9f1f62728a72
Shubham <118046038+shubham4349@users.noreply.github.com>
2025-12-02 18:06:11 +0530

    Time: 18 ms (26.32%) | Memory: 181.9 MB (47.37%) - LeetSync

--- 3526-minimum-number-of-flips-to-make-binary-grid-palindromic-i/minimum-number-of-flips-to-make-binary-grid-palindromic-i.cpp
+++ 3526-minimum-number-of-flips-to-make-binary-grid-palindromic-i/minimum-number-of-flips-to-make-binary-grid-palindromic-i.cpp
@@ -6,33 +6,24 @@ public:
         // pehle row ko pali banao, and steps cnt kr
         // then col ko similarly cnt
         // he return the minm cnt
-        vector<vector<int>> gridd=grid;
-        int rowcnt=0;
+        // vector<vector<int>> gridd=grid;
+        int rc=0,cc=0;
         for(int i=0; i<r; i++){
             int x=0;
             int y=c-1;
             while(x<=y){
-                if(grid[i][x]!=grid[i][y]){
-                    // grid[i][x]=grid[i][y];
-                    // grid[i][y]=grid[i][x];
-                    rowcnt++;  
-                }
+                if(grid[i][x]!=grid[i][y]) rc++;
                 x++;y--;
             }
         }
-        int colcnt=0;
         for(int j=0; j<c; j++){
             int x=0;
             int y=r-1;
             while(x<=y){
-                if(grid[x][j]!=grid[y][j]){ 
-                //    grid[x][j]=grid[y][j];
-                //    grid[y][j]=grid[x][j];
-                    colcnt++;  
-                }
+                if(grid[x][j]!=grid[y][j])cc++;
                 x++;y--;
             }
         }
-        return min(rowcnt,colcnt);
+        return min(cc,rc);
     }
 };

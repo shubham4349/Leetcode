a27def486152aff511d8e3741b18494e98026c25
Shubham <118046038+shubham4349@users.noreply.github.com>
2025-12-03 14:08:12 +0530

    Time: 0 ms (100.00%) | Memory: 9.3 MB (54.06%) - LeetSync

---
+++ 205-isomorphic-strings/isomorphic-strings.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+        int n=s.size();
+        if(s.size()!=t.size()){
+            return false;
+        }
+        map<char,char> mp1,mp2;
+        for(int i=0; i<n; i++){
+            mp1[s[i]]=t[i]; // key-s[i]
+            mp2[t[i]]=s[i];// key=t[i]
+        }
+        for(int i=0; i<n; i++){
+            if(mp1.count(s[i])){
+                if(mp1[s[i]]!=t[i]) return false;
+            }
+            if(mp2.count(t[i])){
+                if(mp2[t[i]]!=s[i]) return false;
+            }
+        } 
+        return true;
+    }
+};

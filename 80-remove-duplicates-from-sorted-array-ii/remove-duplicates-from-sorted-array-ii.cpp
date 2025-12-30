a0b43ec3e0fef59b9c040a3d22a42af9896fd407
Shubham <118046038+shubham4349@users.noreply.github.com>
2025-12-30 16:11:51 +0530

    Time: 10 ms (38.38%) | Memory: 21.2 MB (5.06%) - LeetSync

--- 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.cpp
+++ 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.cpp
@@ -4,17 +4,18 @@ public:
         int n=nums.size();
         map<int,int> cnt;
         for(int num:nums) cnt[num]++;
-        vector<int> ans;
+       // vector<int> ans;
         // if cnt>=2 to do hi dalo...nhi to their cnt
+        nums.clear();
         for(auto &[k,v]:cnt){
             if(v>=2){
-                ans.push_back(k);
-                ans.push_back(k);
+                nums.push_back(k);
+                nums.push_back(k);
             }else {
-                ans.push_back(k);
+                nums.push_back(k);
             }
         }
-        nums.assign(ans.begin(),ans.end());
+      //  nums.assign(ans.begin(),ans.end());
         return nums.size();
     }
 };

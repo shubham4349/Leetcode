a2e6b634ab268c21a6a5304daa3df09235c953af
Shubham <118046038+shubham4349@users.noreply.github.com>
2026-01-02 13:43:38 +0530

    Time: 35 ms (5.38%) | Memory: 35.1 MB (6.44%) - LeetSync

--- 1001-n-repeated-element-in-size-2n-array/n-repeated-element-in-size-2n-array.cpp
+++ 1001-n-repeated-element-in-size-2n-array/n-repeated-element-in-size-2n-array.cpp
@@ -3,10 +3,10 @@ public:
     int repeatedNTimes(vector<int>& nums) {
         int n=nums.size()/2;
         map<int,int> mp;
-        for(int num:nums) mp[num]++;
-        for(auto &m:mp){
-            if(m.second==n) return m.first;
+        for(int num:nums){
+            mp[num]++;
+            if(mp[num]==n) return num;
         }
-        return 0;
+        return -1;
     }
 };

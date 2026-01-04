98979f581291090ac5a7c3d93fd2e228fecbdca2
Shubham <118046038+shubham4349@users.noreply.github.com>
2026-01-04 20:55:11 +0530

    Time: 0 ms (100.00%) | Memory: 13 MB (13.32%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.cpp
+++ 496-next-greater-element-i/next-greater-element-i.cpp
@@ -1,27 +1,31 @@
 class Solution {
 public:
-    int maxRight(vector<int>& arr2, int j) {
-        for (int i = j + 1; i < arr2.size(); i++) {
-            if (arr2[i] > arr2[j]) {
-                return arr2[i];
-            }
-        }
-        return -1;
-    }
-
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-        vector<int> temp;
+        int n1=nums1.size(), n2=nums2.size();
+        // normal version
+        // arr= 1,3,4,2
+        // -> sabka next greater find kr
+        // ans= 3,4,-1,-1 
 
-        for (int i = 0; i < nums1.size(); i++) {
-            for (int j = 0; j < nums2.size(); j++) {
-                if (nums1[i] == nums2[j]) {
-                    int nextGreater = maxRight(nums2, j);
-                    temp.push_back(nextGreater);
-                    break; 
-                }
-            }
+        // for(int i=n-1; i>=0; i--){ // nge in right(so move r->l)
+        //     if(!st.empty() and st.top()<=arr[i])st.pop
+        //  ans[i] = st.empty()? -1:st.top();
+        //     st.push(arr[i]);
+        // } 
+        map<int,int> mp;
+        stack<int> st;
+        for(int i=n2-1; i>=0; i--){
+            while(!st.empty() and nums2[i]>=st.top()){
+                 // we want the top element to be maxm
+                 // monotonic-> either increasing or decre..
+                 st.pop(); 
+            } 
+            mp[nums2[i]]=st.empty()?-1:st.top();
+            // -1 means koi element bada nhi h right me
+            st.push(nums2[i]);
         }
-
-        return temp;
+        vector<int> ans;
+        for(int num:nums1) ans.push_back(mp[num]);
+        return ans;
     }
 };

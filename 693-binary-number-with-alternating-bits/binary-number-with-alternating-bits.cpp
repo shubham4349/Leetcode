45846492330616938ec25a09b3efafc452dbd50b
Shubham <118046038+shubham4349@users.noreply.github.com>
2026-01-19 19:09:29 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (65.10%) - LeetSync

---
+++ 693-binary-number-with-alternating-bits/binary-number-with-alternating-bits.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool hasAlternatingBits(int n) {
+        // concept
+        // ex= 101010101....num1
+        // after right shifting 1 , n>>1 
+        //     010101010 ...num2
+        // num1 xor num2 loge to ->111111
+        // so agar alternating bits rhhega to uska right shit xor ke bad 1 aayega 
+      //  so last me check kro agar all ones bacha then true
+
+     unsigned int x= n^(n>>1);
+
+      if((x&(x+1))==0)return true;
+      // and-> multiply 
+      return false;
+
+    }
+};

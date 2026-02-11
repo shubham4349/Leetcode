00361ab7953ffbbba1e6d3d683184a3b15989bc9
Shubham <118046038+shubham4349@users.noreply.github.com>
2026-02-11 17:54:45 +0530

    Time: 4 ms (22.24%) | Memory: 100.3 MB (66.33%) - LeetSync

--- 932-monotonic-array/monotonic-array.cpp
+++ 932-monotonic-array/monotonic-array.cpp
@@ -1,13 +1,28 @@
 class Solution {
 public:
     bool isMonotonic(vector<int>& nums) {
-          vector<int> inc=nums;
-          vector<int> dec=nums;
+        //   vector<int> inc=nums;
+        //   vector<int> dec=nums;
 
-          sort(begin(inc),end(inc));
-          sort(begin(dec),end(dec),greater<int>());
-          if(nums==inc or nums==dec) return true;
-          return false;
+        //   sort(begin(inc),end(inc));
+        //   sort(begin(dec),end(dec),greater<int>());
+        //   if(nums==inc or nums==dec) return true;
+        //   return false;
+
+         // method-2
+         // assume inc
+
+         int n=nums.size();
+         bool inc=true;
+         for(int i=0; i<n-1; i++){
+              if(nums[i]>nums[i+1]) inc=false;
+         }
+          bool dec=true;
+         for(int i=0; i<n-1; i++){
+              if(nums[i]<nums[i+1]) dec=false;
+         }
+         if(inc==false and dec==false) return false;
+         return true;
 
     }
 };

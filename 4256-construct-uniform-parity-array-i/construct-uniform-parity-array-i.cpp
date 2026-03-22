53a2f366a596f6c9420e81ec8c5b2d59f83d8bfd
Shubham <118046038+shubham4349@users.noreply.github.com>
2026-03-22 23:40:36 +0530

    Time: 0 ms (100.00%) | Memory: 30.2 MB (95.28%) - LeetSync

---
+++ 4256-construct-uniform-parity-array-i/construct-uniform-parity-array-i.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool uniformArray(vector<int>& nums1) {
+        int n=nums1.size();
+        // ans ke sare ele ya to even ya to odd hone chahiye
+        // agar sare element even-> true
+        // sare odd-> true
+        // hybrid ke case me subtract krke unhe uniform banado
+        return true;
+
+    }
+};

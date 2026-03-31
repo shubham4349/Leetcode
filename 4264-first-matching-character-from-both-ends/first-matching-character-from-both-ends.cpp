a0a533e2a758ef3e827bf60a3172f93dbdc83f80
Shubham <118046038+shubham4349@users.noreply.github.com>
2026-03-31 15:36:59 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (67.00%) - LeetSync

---
+++ 4264-first-matching-character-from-both-ends/first-matching-character-from-both-ends.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int firstMatchingIndex(string s) {
+         int n=s.size();
+         int ans=-1;
+         for(int i=0; i<n; i++){
+             if(s[i]==s[n-i-1]){
+                ans=i;
+                break;
+             }
+         }
+         return ans;
+    }
+};

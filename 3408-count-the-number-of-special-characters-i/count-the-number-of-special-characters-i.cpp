b5f3d1c3732da450e809a6a14834cfa4258a2ccf
Shubham <118046038+shubham4349@users.noreply.github.com>
2026-05-27 11:36:38 +0530

    Time: 3 ms (29.63%) | Memory: 8.9 MB (79.40%) - LeetSync

--- 3408-count-the-number-of-special-characters-i/count-the-number-of-special-characters-i.cpp
+++ 3408-count-the-number-of-special-characters-i/count-the-number-of-special-characters-i.cpp
@@ -1,15 +1,30 @@
 class Solution {
 public:
     int numberOfSpecialChars(string word) {
-        int n= word.length();
-        unordered_map<char,int> map;
+        // method 3-> using only vectors
 
-        for(int i=0; i<n; i++){
-            map[word[i]]++;
+        int n= word.length();
+      //  vector<int> lower(27,0); // 0-> 27 , sabme 0
+        // jaha char  milega less say b=2 o lower[2]=1;
+        vector<int> upper(27,0);
+        for(char ch:word){
+             if(ch>='A' and ch<='Z'){
+                int idx=ch-'A'; // B-A = 1, hm isme +1 krke B=2 banate h
+                idx+=1; 
+                upper[idx]=1;
+             }
         }
+        // set<char> seen;
         int cnt=0;
-        for(char ch= 'a' ; ch<='z'; ch++){
-            if(map.count(ch) and map.count(toupper(ch))) cnt++;
+        for(int i=0; i<n; i++){
+            if(word[i]>='a' and word[i]<='z') {
+                    int idx=word[i]-'a';
+                    idx++; // a-a=0 to hm usko a=1 banane ke liye +1 kiye
+                    if(upper[idx]==1){
+                        cnt++;
+                        upper[idx]=0; // ek lower case ke liye ek hi bar cnt kr lwde
+                    } 
+            }
         }
         return cnt;
     }

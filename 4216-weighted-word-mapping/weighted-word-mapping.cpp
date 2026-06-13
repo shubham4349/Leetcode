4572448605112a1a6b2e5828add74112b80e47ea
Shubham <118046038+shubham4349@users.noreply.github.com>
2026-06-13 11:49:09 +0530

    Time: 15 ms (5.44%) | Memory: 47 MB (6.38%) - LeetSync

--- 4216-weighted-word-mapping/weighted-word-mapping.cpp
+++ 4216-weighted-word-mapping/weighted-word-mapping.cpp
@@ -1,27 +1,27 @@
 class Solution {
 public:
     string mapWordWeights(vector<string>& words, vector<int>& weights) {
-        map<int,char> alphaMap;
-        int i=0;
-        for(char ch='z'; ch>='a'; ch--){
-            alphaMap.insert({i,ch});
-            i++;
-        }
-        map<char,int> wordsMap;
-        int j=0;
-        for(char ch='a'; ch<='z'; ch++){
-            wordsMap.insert({ch,weights[j]});
-            j++;
-        }
-        string ans="";
-        for(auto& s:words){
-            int sum=0;
-            for(char& ch:s){
-              sum+=wordsMap[ch];
-            }
-            sum=sum%26;
-            ans+=alphaMap[sum];
-        }
-        return ans;
+       map<char,int> mp;
+       char ch='a';
+       for(int i=0; i<weights.size(); i++){
+           if(ch<='z')  mp.insert({ch,weights[i]%26});
+            ch++;
+       }
+       map<int,char> alpha;
+       char chh='z';
+       for(int i=0; i<26; i++){
+          if(chh>='a')  alpha.insert({i,chh});
+            chh--;
+       }
+       string ans="";
+       for(auto& s: words){
+         int sum=0;
+         for(auto& ch:s){
+           sum+=mp[ch];
+         }
+         sum=sum%26;
+         ans+=alpha[sum];
+       }
+       return ans;
     }
 };

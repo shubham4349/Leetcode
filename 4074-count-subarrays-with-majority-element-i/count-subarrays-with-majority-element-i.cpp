725585007646c7bba449d7bb25efc0f18e47a40d
Shubham <118046038+shubham4349@users.noreply.github.com>
2026-06-25 15:33:25 +0530

    Time: 449 ms (11.11%) | Memory: 421.2 MB (8.93%) - LeetSync

--- 4074-count-subarrays-with-majority-element-i/count-subarrays-with-majority-element-i.cpp
+++ 4074-count-subarrays-with-majority-element-i/count-subarrays-with-majority-element-i.cpp
@@ -1,24 +1,23 @@
 class Solution {
 public:
-    // bool majorityElement(vector<int>& nums,int target){
-    //     int cnt=0;
-    //     int n=nums.size();
-    //     for(int num:nums){
-    //         if(num==target) cnt++;
-    //     }
-    //     return cnt>n/2;
-    // }
-    // len= lastidx-first+1-> 5-0+1=6
     int countMajoritySubarrays(vector<int>& nums, int target) {
         int n=nums.size();
-        int ans=0;
-        for(int i=0; i<n; i++){
-             int cnt=0;
-            for(int j=i; j<n; j++){
-               if(nums[j]==target) cnt++;
-               if(cnt>(j-i+1)/2) ans++;
+        int sub_arr_cnt=0;
+
+        for(int i=0; i<n; i++){ // max-1e3
+         vector<int> temp;
+         int target_cnt=0;
+         int size=0;
+
+            for(int j=i; j<n; j++){ // max-1e3
+                temp.push_back(nums[j]);
+                size++;
+                if(nums[j]==target) target_cnt++;
+
+                if(target_cnt>size/2) sub_arr_cnt++;
             }
+            temp={}; 
         }
-        return ans;
+        return sub_arr_cnt;
     }
 };

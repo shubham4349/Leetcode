54befd151a8d312ce114637101673d9c50e4a797
Shubham <118046038+shubham4349@users.noreply.github.com>
2026-07-07 20:18:18 +0530

    Time: 0 ms (100.00%) | Memory: 9.3 MB (16.67%) - LeetSync

--- 4135-concatenate-non-zero-digits-and-multiply-by-sum-i/concatenate-non-zero-digits-and-multiply-by-sum-i.cpp
+++ 4135-concatenate-non-zero-digits-and-multiply-by-sum-i/concatenate-non-zero-digits-and-multiply-by-sum-i.cpp
@@ -1,20 +1,18 @@
+#define ll long long
 class Solution {
 public:
     long long sumAndMultiply(int n) {
-        string s="";
-        // int len=0;
-        long long sum=0;
-        while(n>0){
-            int digit=n%10;
-            if(digit!=0){
-                sum+=digit;
-                s+=to_string(digit);
+        ll x=0,sum=0;
+        string sx="";
+        if(n==0) return 0;
+        string nx=to_string(n);
+        for(auto& ch:nx){
+            if(ch>'0'){
+                sx+=ch;
+                sum+=ch-'0';
             }
-            n=n/10;
-        }
-        if(s.empty()) return 0;
-        reverse(s.begin(),s.end());
-        long long ans=stoll(s);
-        return ans*sum;
+        }    
+        x=stoll(sx);
+        return sum*x;
     }
 };

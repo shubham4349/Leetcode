522415ae90bdec19f8e15a982b85b818b434b0be
Shubham <118046038+shubham4349@users.noreply.github.com>
2026-08-26 11:05:53 +0530

    Time: 3 ms (80.04%) | Memory: 19.5 MB (30.17%) - LeetSync

--- 1469-minimum-number-of-steps-to-make-two-strings-anagram/minimum-number-of-steps-to-make-two-strings-anagram.cpp
+++ 1469-minimum-number-of-steps-to-make-two-strings-anagram/minimum-number-of-steps-to-make-two-strings-anagram.cpp
@@ -2,21 +2,41 @@ class Solution {
 public:
     int minSteps(string s, string t) {
         
-        unordered_map<char,int> cnt_s;
-        for(auto& ch:s) cnt_s[ch]++;
+        // unordered_map<char,int> cnt_s;
+        // for(auto& ch:s) cnt_s[ch]++;
 
-         unordered_map<char,int> cnt_t;
-         for(auto& ch:t) cnt_t[ch]++;
+        //  unordered_map<char,int> cnt_t;
+        //  for(auto& ch:t) cnt_t[ch]++;
 
-         int ans=0;
-         for(auto& [ch,cnt]:cnt_s){
-              if(cnt>cnt_t[ch]){
-                 // agar kisi char ki freq in s > t
-                 // to t me diff steps lagenge s ka perm bananae me
-                 ans+= cnt-cnt_t[ch]; // diff
-              }
-         }
-         return ans;
+        //  int ans=0;
+        //  for(auto& [ch,cnt]:cnt_s){
+        //       if(cnt>cnt_t[ch]){
+        //          // agar kisi char ki freq in s > t
+        //          // to t me diff steps lagenge s ka perm bananae me
+        //          ans+= cnt-cnt_t[ch]; // diff
+        //       }
+        //  }
+        //  return ans; 
+
+        // mthod 2- using vectors
+
+        vector<int> freqs(26,0);
+        for(char& ch:s){
+            freqs[ch-'a']++; 
+        }
+        vector<int> freqt(26,0);
+        for(char& ch:t){
+            freqt[ch-'a']++; 
+        }
+        int ans=0;
+        for(int i=0; i<26; i++){
+            if(freqs[i]>freqt[i]){
+                ans+=freqs[i]-freqt[i];
+            }
+        }
+        return ans;
+        
+
 
 
 
